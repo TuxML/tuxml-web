2c666f5270656d2d157eba63a546bc37a5c96a96
Guillaume <51230568+tartiflm@users.noreply.github.com>
2020-10-07 18:08:44 +0200

    en théorie le tunnelling ssh marche

--- server.py
+++ server.py
@@ -2,26 +2,55 @@ from flask import Flask
 from flask import render_template
 import os
 import mysql.connector
+from sshtunnel import SSHTunnelForwarder
+import socket
+
+bdip = '148.60.11.195'
+
+tuxmlDB = None
+
+if(socket.gethostname() != 'tuxmlweb'):
+	print("Connexion à la BDD en passant par le serveur web (SSH)")
+	tunn = SSHTunnelForwarder(
+			('tuxmlweb.istic.univ-rennes1.fr', 22),
+			ssh_username='zprojet',
+			ssh_pkey='../keys/cle',
+			remote_bind_address=(bdip, 3306))
+	tunn.start()
+	print("Connecté au serveur web (SSH), connexion à la BDD")
+	tuxmlDB = mysql.connector.connect(
+		host=tunn.local_bind_host,
+		port=tunn.local_bind_port,
+		user='web',
+		password='df54ZR459',
+		database='IrmaDB_result')
+else:
+	print("Connexion directe à la BDD")
+	tuxmlDB = mysql.connector.connect(
+		host=bdip,
+		user='web',
+		password='df54ZR459',
+		database='IrmaDB_result')
+
+print("Connecté !")
+
 app = Flask(__name__, template_folder=os.path.abspath('templates'))
-    
+
+
 @app.route('/')
 def hello_world():
-	tuxmlDB = mysql.connector.connect(
-        host='148.60.11.195',
-        user='web',
-        password='df54ZR459',
-        database='IrmaDB_result')
-
 	mycursor = tuxmlDB.cursor()
 	mycursor.execute("SELECT COUNT(*) FROM compilations")
 	nbcompil = mycursor.fetchone()[0]
 
-	return render_template('base.html',count=nbcompil)
+	return render_template('base.html', count=nbcompil)
+
 
 @app.route('/test1')
 def hello():
-    return 'Hello, World!'
+	return 'Hello, World!'
+
 
 if __name__ == "__main__":
 	from waitress import serve
-	serve(app, host="127.0.0.1", port=8000)
+serve(app, host="127.0.0.1", port=8000)

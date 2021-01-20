45a4588cbccec8c25d5aa18b89e903e4b236da66
Jean <jean.derieux@gmail.com>
2021-01-20 16:22:19 +0100

    Added gcc column

--- server.py
+++ server.py
@@ -102,16 +102,31 @@ def data():
     str_interest = ""
     url_interest = ""
     for e in interest:
-        str_interest = str_interest + ", " + e + " "
+        str_interest = str_interest + ", compilations." + e + " "
         url_interest = url_interest + "&interest=" + e
+
+
+    interest_software = request.args.getlist('interest_software')
+    str_interest_software = ""
+    url_interest_software = ""
+    str_interest_software_left_join = ""
+    for e in interest_software :
+        str_interest_software = str_interest_software + ", software_environment." + e + " "
+        url_interest_software = url_interest_software + "&interest=" + e
+
+    if interest_software is not None :
+        str_interest_software_left_join = " LEFT JOIN software_environment ON compilations.sid = software_environment.sid "
+
 
 
 
 
     versions = [["All"]] + dbManager.getExistingKernelVersions()
-    temp = dbManager.makeRequest("SELECT b.* FROM (SELECT a.* FROM (SELECT cid " + str_interest + " FROM compilations " + ("" if laversion == "All" else f"WHERE compiled_kernel_version = '{laversion}'")+ f" ORDER BY {sortBy} {'ASC' if ascend else 'DESC'} LIMIT " + str(numberOfNupletTemp) + f")a ORDER BY {sortBy} {'DESC' if ascend else 'ASC'} LIMIT  " +  str(numberOfNuplet) + f")b ORDER BY {sortBy} {'ASC' if ascend else 'DESC'} ;")
+    temp = dbManager.makeRequest("SELECT b.* FROM (SELECT a.* FROM (SELECT compilations.cid " + str_interest + str_interest_software + " FROM compilations " + str_interest_software_left_join + ("" if laversion == "All" else f"WHERE compiled_kernel_version = '{laversion}'")+ f" ORDER BY {sortBy} {'ASC' if ascend else 'DESC'} LIMIT " + str(numberOfNupletTemp) + f")a ORDER BY {sortBy} {'DESC' if ascend else 'ASC'} LIMIT  " +  str(numberOfNuplet) + f")b ORDER BY {sortBy} {'ASC' if ascend else 'DESC'} ;")
     count = dbManager.getCompilationCount(laversion)
-    
+
+
+
     #modify the values contained in the query to adapt the reading to a human
     ten=[]
     line = []
@@ -120,7 +135,7 @@ def data():
         for e in row :
             if i == -1:
                 line = [str(e)]
-            else:
+            elif i < len(interest):
                 if interest[i] == "compiled_kernel_size" :
                     if e == -1:
                         line.append("Compilation failed")
@@ -131,11 +146,11 @@ def data():
                         line.append('{:02d} min {:02d} sec'.format(int(m), int(s)))
                 else:
             	    line.append(str(e))
+            else :
+                line.append(str(e))
             i = i + 1
         i = -1
         ten.append(line)
-
-
 
     return render_template('data.html', laversion=laversion, numberOfNuplet=numberOfNuplet, page=page, versions=versions, ten=ten, sortBy=sortBy, ascend=ascend, count=count, interest=interest, url_interest=url_interest)
 

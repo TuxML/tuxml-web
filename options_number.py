dd3b95bcf81b64d1c598ab0dc733473117662af3
ziko <z.boukhchen@gmail.com>
2021-03-10 19:07:59 +0100

    correction of the number of options "only active" + the comparison between the different averages of all versions "comparaison des moyennes d'options de toutes les version"

--- options_number.py
+++ options_number.py
@@ -2,6 +2,10 @@ import bz2
 import mysql.connector
 import socket
 import sys
+import statistics
+# Use: statistics.mean(liste) 
+from statistics import mean
+# Use: mean(liste)
 from os import path
 def get_configuration_file(cid):
     #print("Getting configuration file of cid=", cid)
@@ -31,7 +35,7 @@ def get_configuration_file(cid):
         socket.close()
 #cf1 = get_configuration_file(116733)
 
-#return 10 last cid of correctly compiled version 5.0  
+#return 10 last cid of correctly compiled version v  
 def get_list_of_cid_for_version(v):
     v_liste = [4.13, 4.15, 4.16, 4.17, 4.18, 4.20, 5.0, 5.1, 5.10, 5.4, 5.7, 5.8, 5.9]
     x=[]
@@ -48,7 +52,7 @@ def get_list_of_cid_for_version(v):
         #create a cursor    
         cursor = socket.cursor()
         #sql query
-        query = "SELECT cid FROM compilations WHERE compiled_kernel_version = 5.0 and compiled_kernel_size > 0 order by cid desc limit 10"
+        query = "SELECT cid FROM compilations WHERE compiled_kernel_version = %f and compiled_kernel_size > 0 order by cid desc limit 10" %v
         cursor.execute(query)
         l = cursor.fetchall()
         for row in l:
@@ -67,17 +71,33 @@ def get_list_of_cid_for_version(v):
 #y=get_list_of_cid_for_version(5.0)     
 #print(y)
 
+#return number of options in a specified file
 def options_number(cpt):
             j=0 
             for i, line in enumerate(cpt):
-                
-                if '#' not in line:
+                # on ne concidere que les options valides à 'y' ou 'm' ou des valeurs entière/string
+                if line.startswith('C'):
                     j +=1
             return j 
-
+#return a list of number of options
 def options(x):
             l =[]
             for i in x:
                 l.append(options_number(get_configuration_file(i)))
-            return l       
-#print(options(get_list_of_cid_for_version(5.0))
+            return l  
+#return la moyennes des options pour une version donnée            
+def moyenne(y):
+            m = mean(y)
+            return m  
+#return la liste des moyennes de toutes les versions            
+def liste_de_moyenne():
+    v_liste = [4.13, 4.15, 4.16, 4.17, 4.18, 4.20, 5.0, 5.1, 5.10, 5.4, 5.7, 5.8, 5.9] 
+    length = len(v_liste)    
+    l = []
+    i=0
+    while i < length:
+        x = get_list_of_cid_for_version(v_liste[i])
+        y = options(x)
+        l.append(moyenne(y))
+        i += 1
+    return l    

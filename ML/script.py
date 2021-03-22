fddc08dc4ad92e09e60bbce9e6d8465ee4a54444
Jean <jean.derieux@gmail.com>
2021-03-22 15:54:12 +0100

    Adapt path to zprojet

--- ML/script.py
+++ ML/script.py
@@ -8,6 +8,13 @@ import numpy as np
 
 version = '4.15'
 number = 1
+path = "/home/zprojet/tuxml-web/ML"
+
+try:
+    os.chdir(path)
+except OSError:
+    print ("Failure Change current working directory to : %s " % path)
+    sys.exit()
 
 for i in range(number):
     getConfig(version)

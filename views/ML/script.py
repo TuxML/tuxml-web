bd23170f972d011ccef031924f8b17c7fc88e0a3
Jean <jean.derieux@gmail.com>
2021-03-25 17:00:54 +0100

    Update downloading script

--- views/ML/script.py
+++ views/ML/script.py
@@ -9,19 +9,7 @@ from .growML import grow
 
 version = "4.15"
 number = 1
-path = "/home/zprojet/tuxml-web/ML"
 
-if os.path.exists(path):
-	pass
-else:
-	path = "."
-
-
-try:
-    os.chdir(path)
-except OSError:
-    print ("Failure Change current working directory to : %s " % path)
-    sys.exit()
 
 for i in range(number):
     getConfig(version)

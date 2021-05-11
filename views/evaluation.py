a1ce34146ee509ac496cabc96554def0b6d8da1d
Jean <jean.derieux@gmail.com>
2021-05-11 16:42:03 +0200

    Fix the error caused by too few compilations for /evaluation

--- views/evaluation.py
+++ views/evaluation.py
@@ -31,25 +31,43 @@ def evaluation_view():
         X = np.load( DOWNLOADS_DIRECTORY_PATH + "/" + version + "/X.npy")
         y = np.load( DOWNLOADS_DIRECTORY_PATH + "/" + version + "/y.npy")
 
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
-        train_set = X_train.shape
-        test_set = X_test.shape
+        shape = X.shape
+
+        #we check if there is enough data
+        if shape[0] > 5 :
+
+            X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+            train_set = X_train.shape
+            test_set = X_test.shape
 
 
-        #KNeighborsClassifier
-        modelKNC = KNeighborsClassifier()
-        modelKNC.fit(X_train, y_train)
+            #KNeighborsClassifier
+            modelKNC = KNeighborsClassifier()
+            modelKNC.fit(X_train, y_train)
 
-        KNC_train_score = modelKNC.score(X_train, y_train)
-        KNC_test_score = modelKNC.score(X_test, y_test)
+            KNC_train_score = modelKNC.score(X_train, y_train)
+            KNC_test_score = modelKNC.score(X_test, y_test)
 
-        #DecisionTreeClassifier
-        modelDTC = DecisionTreeClassifier()
-        modelDTC.fit(X_train, y_train)
+            #DecisionTreeClassifier
+            modelDTC = DecisionTreeClassifier()
+            modelDTC.fit(X_train, y_train)
 
-        DTC_train_score = modelDTC.score(X_train, y_train)
-        DTC_test_score = modelDTC.score(X_test, y_test)
+            DTC_train_score = modelDTC.score(X_train, y_train)
+            DTC_test_score = modelDTC.score(X_test, y_test)
+
+        else:
+
+            train_set = "not enough data"
+            test_set = "not enough data"
+
+            KNC_train_score = "not enough data"
+            KNC_test_score = "not enough data"
+
+            DTC_train_score = "not enough data"
+            DTC_test_score = "not enough data"
+
 
         return render_template('evaluation.html', list_version=list_version, train_set=train_set, test_set=test_set, KNC_train_score=KNC_train_score, KNC_test_score=KNC_test_score, DTC_train_score=DTC_train_score, DTC_test_score=DTC_test_score, version=version)
 
